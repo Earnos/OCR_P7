6687ba3cedd401e32385451590d3527591fd3375
Limoges_Cyril <lcyril33@gmail.com>
2023-01-16 14:28:02 +0100

    responsive adjustements

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -2,11 +2,11 @@ import PropTypes from "prop-types"
 
 function Tags({tags}) { 
         return (
-        <>
+        <div className="tags">
         { tags.map((tag, index) =>(
         <span  key={index} className="logement-info1-tags" >{tag}</span>
         ))}
-        </>
+        </div>
     )     
 }
 
